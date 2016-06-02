db00c44103d56b502cec5f76181f63ac20b40809
vivictor14 <victor.vogel@hotmail.fr>
2016-06-02 19:08:27 +0200

    Update errer.pl

--- errer.pl
+++ errer.pl
@@ -6,12 +6,14 @@
 
 % Se diriger vers le point inexploré le plus proche
 % A droite
-errer(L, X, Y, Pos, Size, VPx, VPy, 1) :- nb_getval(labyrinthe, Laby), premiereOccurence(Laby, X, Y, -1, X1, Y1), seDirigerVers(L, X, Y, Pos, Size, X1, Y1, 1).
+errer(L, X, Y, Pos, Size, VPx, VPy, 1) :- nb_getval(labyrinthe, Laby), premiereOccurenceDroite(Laby, X, Y, -1, X1, Y1), seDirigerVers(L, X, Y, Pos, Size, X1, Y1, 1).
 % En haut
-errer(L, X, Y, Pos, Size, VPx, VPy, 2) :- nb_getval(labyrinthe, Laby), premiereOccurence(Laby, X, Y, -1, X1, Y1), seDirigerVers(L, X, Y, Pos, Size, X1, Y1, 2).
+errer(L, X, Y, Pos, Size, VPx, VPy, 2) :- nb_getval(labyrinthe, Laby), premiereOccurenceHaut(Laby, X, Y, -1, X1, Y1), seDirigerVers(L, X, Y, Pos, Size, X1, Y1, 2).
 % A gauche
-errer(L, X, Y, Pos, Size, VPx, VPy, 3) :- nb_getval(labyrinthe, Laby), premiereOccurence(Laby, X, Y, -1, X1, Y1), seDirigerVers(L, X, Y, Pos, Size, X1, Y1, 3).
+errer(L, X, Y, Pos, Size, VPx, VPy, 3) :- nb_getval(labyrinthe, Laby), premiereOccurenceGauche(Laby, X, Y, -1, X1, Y1), seDirigerVers(L, X, Y, Pos, Size, X1, Y1, 3).
 % En bas
-errer(L, X, Y, Pos, Size, VPx, VPy, 4) :- nb_getval(labyrinthe, Laby), premiereOccurence(Laby, X, Y, -1, X1, Y1), seDirigerVers(L, X, Y, Pos, Size, X1, Y1, 4).
+errer(L, X, Y, Pos, Size, VPx, VPy, 4) :- nb_getval(labyrinthe, Laby), premiereOccurenceBas(Laby, X, Y, -1, X1, Y1), seDirigerVers(L, X, Y, Pos, Size, X1, Y1, 4).
 
-% 
+% Trouver la première occurence d'un élément à droite du mineur
+premiereOccurenceDroite([L|Laby], X, 0, Element, X1, Y1) :- member(-1, L).
+premiereOccurenceDroite([L|Laby], X, Y, Element, X1, Y1) :- premiereOccurence(Laby, X, Y, Element, X1, Y1).
