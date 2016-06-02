c1b62c88f4372df60b88d5f6d75511fffa040ad7
Victor Vogel <Victor Vogel>
2016-06-02 13:52:46 +0200

    Ajout positionLaPlusProche

--- decision.pl
+++ decision.pl
@@ -29,18 +29,20 @@ init(_).
 % move( Action ) :- Action is 1+random( 4 ), write(Action),nl.
 
 % Se diriger vers la sortie
-move(L, _, X, Y, Pos, Size, 0, _, _, VPx, VPy, Action) :- member(20, L), positionLaPlusProche(20, Pos1), seDirigerVers(L, Pos, Size, Pos1, Action).
+move(L, _, X, Y, Pos, Size, 0, _, _, VPx, VPy, Action) :- member(20, L), positionLaPlusProche(L, Size, Pos, 20, Pos1), seDirigerVers(L, Pos, Size, Pos1, Action).
 
 % Se diriger vers un diamant
-move(L, _, X, Y, Pos, Size, 1, _, _, VPx, VPy, Action) :- member(2, L), positionLaPlusProche(2, Pos1), seDirigerVers(L, Pos, Size, Pos1, Action).
+move(L, _, X, Y, Pos, Size, 1, _, _, VPx, VPy, Action) :- member(2, L), positionLaPlusProche(L, Size, Pos, 2, Pos1), seDirigerVers(L, Pos, Size, Pos1, Action).
 
 % Errer
 move(L, _, X, Y, Pos, Size, _, _, _, VPx, VPy, Action) :- errer(L, X, Y, Pos, Size, VPx, VPy, Action).
 
-% Position de la première occurence de l'élément
-position(Element, [X|_], Pos, Index) :- Element = X, Pos = Index, !.
-position(Element, [X|R], Pos, Index) :- Pos1 is Pos + 1, position(Element, R, Pos1, Index).
-position(Element, L, Index) :- position(Element, L, 0, Index).
+% Position la plus proche
+positionLaPlusProche(L, Size, Pos, Element, Pos1) :- allPosition(Element, L, Positions), positionLPP(Pos, Size, Positions, Pos1).
+positionLPP(Pos, Size, Positions, Pos1) :- positionLPP(Pos, Size, Positions, Size, Pos1).
+positionLPP(_, _, [], _, _).
+positionLPP(Pos, Size, [X|R], PosInit, PosPP) :- abs(Pos - (X mod Size)) <= PosInit, PosInit is X, PosPP is X, positionLPP(Pos, Size, R, PosInit, PosPP).
+positionLPP(Pos, Size, [X|R], PosInit, PosPP) :- positionLPP(Pos, Size, R, PosInit, PosPP).
 
 % Positions de toutes les occurences de l'élément
 allPosition(_, [], _, []).
