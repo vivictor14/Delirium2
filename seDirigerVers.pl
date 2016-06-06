e1b0ccfb3fe024b7711fb4a4b93e0dc8096a7e90
Zaxyh <zaxyh@hotmail.fr>
2016-06-07 01:34:24 +0200

    On a fait des trucs de ouf

--- seDirigerVers.pl
+++ seDirigerVers.pl
@@ -1,167 +1,276 @@
 :- module( seDirigerVers, [
-	init/1,
+	init_astar/1,
 	seDirigerVers/5
 ] ).
 
 % Init variables globales
-init(_):- nb_setval(openList,[]), nb_setval(closedList,[]).
+init_astar(_):- nb_setval(openList,[]), nb_setval(closedList,[]).
 
 % Predicats A*
 /* 
-  a_star(+CourantState, +FinalState, -Path) 
+  a_star(+CourantState, +FinalState, +Labyrinth, -Path) 
 */
 % openList vide pas de solution
-a_star(_,_,_,[],_,_):-
+a_star(_,_,_,_):-
 	nb_getval(openList, []),
 	!.
 	
 % etat final atteint
-a_star(_,_,_,[X,Y],[X,Y],_):-
-	nb_getval(openList, [[_], [X,Y]])
+a_star([X,Y],[X,Y],_,Path):-
+	buildPath(Path).
 
 
-a_star(Liste,PosMineur,Size,[X,Y],[XFinal,YFinal],Path):-
-
-	nb_getval(openList, [[Chemin,G,Cout,Pere]|CheminRestant]),
-	nb_getval(closedList, ClosedList),
-	
-	Chemin = [X,Y],
-	nb_setval(openList,CheminRestant),
-	append(closedList,[Chemin],closedListFinal),
-	nb_setval(closedList,closedListFinal),
-	trouverSuccesseur(PosMineur,Chemin,Size,Liste,Successeur),
-	ajouterChemin(Chemin,Successeur,[XFinal,YFinal],Cout,G),
-	nb_getval(openList, [[Chemin,_,_,_]|_]),
-	a_star(Liste,[X,Y],Chemin,[XFinal,YFinal],Path).
+a_star([X,Y],[XFinal,YFinal],Laby,Path):-
+	extractBestNodeFromOpenList(Node),
+	addNodeToClose(Node),
+	Node = [[X,Y],G,F,_],
+	trouverSuccesseurs([X,Y],Laby,Successeurs),
+	ajouterChemin([X,Y],Successeurs,[XFinal,YFinal],G),
+	/*nb_getval(openList, [[NewState,_,_,_]|_]),
+	a_star(NewState,[XFinal,YFinal],Laby,Path).*/
 	
 	
-ajouterChemin(_,[],_,_,_).	
+ajouterChemin(_,[],_,_,_).
 % test si il n'est pas dans closed et open
-ajouterChemin(Chemin,[Successeur|Reste],Fin,Cout,G):-
-	nb_getval(openList, openList),
+ajouterChemin([X,Y],[Successeur|Reste],Fin,G):-
+	nb_getval(openList, OpenList),
 	nb_getval(closedList, ClosedList),
-	nonOpen(Successeur,openList),
-	not(member(Successeur,closeList)),
-	getHeuristicValue(Successeur,Fin,Y),
-	F is G + 1 + Y,
-	NouvelleDonnee is [Successeur,Y,F,Chemin],
-	% ajout trié !
+	nonStateInListe(Successeur,OpenList),
+	nonStateInListe(Successeur,CloseList),
+	!,
+	getHeuristicValue(Successeur,Fin,H),
+	GPlus is (G + 1),
+	F is (GPlus + H),
+	NewNode = [Successeur,GPlus,F,[X,Y]],
+	addNodeToOpen(NewNode),
+	ajouter([X,Y],Reste,Fin,G).
 	
 % test si g(y) > g(N.e)+c(N.e,y)
-ajouterChemin(Chemin,[Successeur|Reste],Fin,Cout,G):-
+ajouterChemin([X,Y],[Successeur|Reste],Fin,G):-
+	testBestCostInOpenOrClose([X,Y],Successeur,G,Fin),
+	!,
+	ajouterChemin([X,Y],Reste,Fin,G).
 
 % aucun des deux critères n'est possible, ont passe au successeur suivant
-ajouterChemin(Chemin,[_|Reste],Fin,Cout,G):-
-	ajouterChemin(Chemin,Reste,Fin,Cout,G).
+ajouterChemin([X,Y],[_|Reste],Fin,G):-
+	ajouterChemin([X,Y],Reste,Fin,G).
 
-nonOpen(_,[]).
-nonOpen(Successeur,[[Chemin,_,_,_]|CheminRestant]):-
-	Successeur \= Chemin,
-	nonOpen(Successeur,CheminRestant).
+nonStateInListe(_,[]).
+nonStateInListe(Successeur,[[State,_,_,_]|StateRestant]):-
+	Successeur \= State,
+	nonStateInListe(Successeur,StateRestant).
 	
 	
-
-/* 
-  getBestNodeFromOpenList(-Node) 
-*/
-getBestNodeFromOpenList(_).
-
 % trouver les successeur 
-trouverSuccesseur(PosMineur,Chemin,Size,Liste,Successeur):-
-	movementD(PosMineur,Liste,Chemin,X1),
-	movementH(PosMineur,Liste,Size,Chemin,X2),
-	movementG(PosMineur,Liste,Size,Chemin,X3),
-	movementB(PosMineur,Liste,Chemin,X4),
-	append(X1,X2,Successeur),
-	append(X3,Successeur,Successeur),
-	append(X4,Successeur,Successeur).
+trouverSuccesseurs([X,Y],Laby,Successeurs):-
+	movementD([X,Y],Laby,CoordD),
+	movementH([X,Y],Laby,CoordH),
+	movementG([X,Y],Laby,CoordG),
+	movementB([X,Y],Laby,CoordB),
+	Successeurs = [CoordD,CoordH,CoordG,CoordB].
 
-movementD(PosMineur,List,Chemin,X):-
-	Chemin = [XChemin,YChemin],
-	Pos is PosMineur +1,
-	possibleMove(List,Pos),
-	XNew is XChemin+1,
-	X is [XNew,Y].
-movementD(PosMineur,List,Chemin,X):-
-	X is [].
-	
-movementH(PosMineur,List,Size,Chemin,X):-
-	Chemin = [XChemin,YChemin],
-	Pos is PosMineur - Size,
-	possibleMove(List,Pos),
-	YNew is YNew+1,
-	X is [X,YNew].
-movementH(PosMineur,List,Chemin,X):-
-	X is [].
-	
-movementB(PosMineur,List,Chemin,X):-
-	Chemin = [XChemin,YChemin],
-	Pos is PosMineur + Size,
-	possibleMove(List,Pos),
-	YNew is YNew-1,
-	X is [X,YNew].
-movementB(PosMineur,List,Size,Chemin,X):-
-	X is [].
-	
-movementG(PosMineur,List,Chemin,X):-
-	Chemin = [XChemin,YChemin],
-	Pos is PosMineur -1,
-	possibleMove(List,Pos),
-	XNew is XChemin-1,
-	X is [XNew,Y].
-movementG(PosMineur,List,Chemin,X):-
-	X is [].
-	
-/* 
-  extractBestNodeFromOpenList(-Node) 
-*/
-extractBestNodeFromOpenList(_).
+movementD([X,Y],Laby,CoordD):-
+	XNew is X+1,
+	possibleMove([XNew,Y],Laby),
+	CoordD = [XNew,Y].
 
-/* 
-  buildPath(+Fils,+Pere,+PathPrecedent,-NewPath) 
-*/
-buildPath(Fils,_,[],[Fils]).
-buildPath(Fils,Pere,Path,NPath):-
-	Path = [[X,Y]|Reste],
-	Pere = [X,Y],
-	append([Fils],Path,NPath).
-buildPath(Fils,Pere,Path,NPath):-
-	Path = [[X,Y]|Reste],
-	Pere \= [X,Y],
-	buildPath(Fils,Pere,Reste,NPath).
+movementD(_,_,[]).
+	
+movementH([X,Y],Laby,CoordH):-
+	YNew is Y-1,
+	possibleMove([X,YNew],Laby),
+	CoordH = [X,YNew].
 
+movementH(_,_,[]).
+	
+movementB([X,Y],Laby,CoordB):-
+	YNew is Y+1,
+	possibleMove([X,YNew],Laby),
+	CoordB = [X,YNew].
 
-/* 
-  insertAllStatesInOpenList(+Node, +FinalState, +AccessibleStatesList) 
-*/
-insertAllStatesInOpenList(_,_,_).
+movementB(_,_,[]).
+	
+movementG([X,Y],Laby,CoordG):-
+	XNew is X-1,
+	possibleMove([XNew,Y],Laby),
+	CoordG = [XNew,Y].
+
+movementG(_,_,[]).	
 
 % Predicats A* spécifique problème
 /* 
-  getHeuristicValue(+XATester,+YATester,+XFinal,+YFinal,-V)
+  getHeuristicValue([+XATester,+YATester],[+XFinal,+YFinal],-V)
 */
 getHeuristicValue([XATester,YATester],[XFinal,YFinal],V):- V is sqrt((XFinal-XATester)*(XFinal-XATester)+(YATester-YFinal)*(YATester-YFinal)).
 
 /*
-  getAllAccessibleStates(+State, -AccessibleStatesList)
+  extractBestNodeFromOpenList(-Node)
 */
-getAllAccessibleStates(_,_).
+extractBestNodeFromOpenList(Node):-
+	nb_getval(openList,OpenList),
+	extractBestNodeFromOpenList(Node,OpenList,-1),
+	substractFromOpenList(Node).
+	
+extractBestNodeFromOpenList(BestNode,[],_).
+extractBestNodeFromOpenList(BestNode,[BestNode|_],-1).
+extractBestNodeFromOpenList(BestNode,[Node|AutreNode],F):-
+	Node = [_,_,FNode,_],
+	F >= FNode,
+	!,
+	BestNode = Node,
+	extractBestNodeFromOpenList(BestNode,AutreNode,FNode).
+extractBestNodeFromOpenList(BestNode,[_|AutreNode],F):-
+	extractBestNodeFromOpenList(BestNode,AutreNode,F).
+
+/*
+  extractNodeFromOpen(+State, -Node)
+*/
+extractNodeFromOpen(State,Node):-
+	nb_getval(openList,OpenList),
+	extractNodeFromOpen(State,Node,OpenList),
+	substractFromOpenList(S).
+	
+extractNodeFromOpen(_,_,[]):- fail.
+extractNodeFromOpen(State,Node,[Node|_]):-
+	Node = [State,_,_,_].
+extractNodeFromOpen(State,Node,[_|AutreNode]):-
+	extractNodeFromOpen(State,Node,AutreNode).	
+
+/*
+  substractFromOpenList(-Node)
+*/
+substractFromOpenList(Node):-
+	nb_getval(openList,OpenList),
+	substractFromOpenList(Node,OpenList,[]).
+	
+substractFromOpenList(Node,[Node|AutreNode],DebutNode):-
+	append(DebutNode,AutreNode,NewOpenList),
+	nb_setval(openList,NewOpenList).
+substractFromOpenList(Node,[PasNode|AutreNode],DebutNode):-
+	append(DebutNode,PasNode,ListeNode),
+	substractFromOpenList(Node,AutreNode,ListeNode).
+	
+/*
+  addNodeToOpen(+Node)
+*/
+addNodeToOpen(Node):-
+	nb_getval(closeList,CloseList),
+	append([Node],CloseList,NewCloseList),
+	nb_setval(closeList,NewCloseList).
+
+/*
+  extractNodeFromClose(+State, -Node)
+*/
+extractNodeFromClose(State,Node):-
+	nb_getval(closeList,CloseList),
+	extractNodeFromClose(State,Node,CloseList),
+	substractFromCloseList(Node).
+	
+extractNodeFromClose(_,_,[]):- fail.
+extractNodeFromClose(State,Node,[Node|_]):-
+	Node = [State,_,_,_].
+extractNodeFromClose(State,Node,[_|AutreNode]):-
+	extractNodeFromClose(State,Node,AutreNode).
+
+/*
+  substractFromCloseList(+Node)
+*/
+substractFromCloseList(Node):-
+	nb_getval(closeList,CloseList),
+	substractFromCloseList(Node,CloseList,[]).
+	
+substractFromCloseList(Node,[Node|AutreNode],DebutNode):-
+	append(DebutNode,AutreNode,NewCloseList),
+	nb_setval(closeList,NewCloseList).
+substractFromCloseList(Node,[PasNode|AutreNode],DebutNode):-
+	append(DebutNode,PasNode,ListeNode),
+	substractFromCloseList(Node,AutreNode,ListeNode).
+	
+/*
+  addNodeToClose(+Node)
+*/
+addNodeToClose(Node):-
+	nb_getval(closeList,CloseList),
+	append([Node],CloseList,NewCloseList),
+	nb_setval(closeList,NewCloseList).
+	
+/*
+  testBestCostInOpenOrClose(+Pere,+Successeur,+GPere,+Fin)
+*/
+testBestCostInOpenOrClose([X,Y],Successeur,GPere,Fin):-
+	isInOpenWithBestCost(Successeur,GPere),
+	!,
+	extractNodeFromOpen(Successeur,Node),
+	GPlus is (GPere + 1),
+	getHeuristicValue(Successeur,Fin,H)
+	F is (GPlus + H),
+	NewNode = [Successeur,GPlus,F,[X,Y]],
+	addNodeToOpen(NewNode).
+	
+testBestCostInOpenOrClose([X,Y],Successeur,GPere):-
+	isInCloseWithBestCost(Successeur,GPere),
+	!,
+	extractNodeFromClose(Successeur,Node),
+	GPlus is (GPere + 1),
+	getHeuristicValue(Successeur,Fin,H)
+	F is (GPlus + H),
+	NewNode = [Successeur,GPlus,F,[X,Y]],
+	addNodeToOpen(NewNode).
+	
+/*
+  isInOpenWithBestCost(+Successeur,+GPere)
+*/
+isInOpenWithBestCost(Successeur,GPere):-
+	nb_getval(openList,OpenList),
+	isInOpenWithBestCost(Successeur,GPere,OpenList).
+	
+isInOpenWithBestCost(Successeur,GPere,[]):- fail.
+	
+isInOpenWithBestCost(Successeur,GPere,[Node|_]):-
+	Node = [Successeur,G,_,_],
+	GPlus is (GPere + 1),
+	GPlus =< G,
+	!.
+	
+isInOpenWithBestCost(Successeur,GPere,[_|AutreNode]):-
+	isInCloseWithBestCost(Successeur,GPere,AutreNode).
 
 
-% Type de l'élément à la position indiquée
-elementAtPosIs([X|_], 0, X).
-elementAtPosIs([X|R], Pos, Element) :- Pos1 is Pos - 1, elementAtPosIs(R, Pos1, Element).
+	
+/*
+  isInCloseWithBestCost(+Successeur,+GPere)
+*/
+isInCloseWithBestCost(Successeur,GPere):-
+	nb_getval(closeList,CloseList),
+	isInCloseWithBestCost(Successeur,GPere,CloseList).
+	
+isInCloseWithBestCost(Successeur,GPere,[]):- fail.
+	
+isInCloseWithBestCost(Successeur,GPere,[Node|_]):-
+	Node = [Successeur,G,_,_],
+	GPlus is (GPere + 1),
+	GPlus =< G,
+	!.
+	
+isInCloseWithBestCost(Successeur,GPere,[_|AutreNode]):-
+	isInCloseWithBestCost(Successeur,GPere,AutreNode).
+	
+% Element aux coordonnées données
+elemAtCoord([[E|_]|_], 0, 0, E).
+elemAtCoord([[_|R1]|R2], X, 0, E) :- X1 is X - 1, elemAtCoord([R1|R2], X1, 0, E). 
+elemAtCoord([_|R2], X, Y, E) :- Y1 is Y - 1, elemAtCoord(R2, X, Y1, E).
 
 % Prochaine position possible
-possibleMove(L, Pos) :- elementAtPosIs(L, Pos, X), X < 3.
-possibleMove(L, Pos) :- elementAtPosIs(L, Pos, X), X = 20.
+possibleMove([X,Y], Laby) :- elemAtCoord(Laby, [X,Y], E), E = 1.
+possibleMove([X,Y], Laby) :- elemAtCoord(Laby, [X,Y], E), E = 2.
+possibleMove([X,Y], Laby) :- elemAtCoord(Laby, [X,Y], E), E = 21.
 
-% Se rapproche de la position voulue
-seRapproche(Pos, Pos1, Pos2) :- X is abs(Pos2 - Pos), Y is abs(Pos2 - Pos1), X > Y.
+////////////////////////////////////////
 
-% Position la plus proche
-positionLaPlusProche(L, Size, Pos, Element, Pos1) :- allPosition(Element, L, Positions), positionLPP(Pos, Size, Positions, Pos1).
-positionLPP(Pos, Size, Positions, Pos1) :- positionLPP(Pos, Size, Positions, Size, Pos1).
-positionLPP(_, _, [], _, _).
-positionLPP(Pos, Size, [X|R], PosInit, PosPP) :- abs(Pos - (X mod Size)) <= PosInit, PosInit is X, PosPP is X, positionLPP(Pos, Size, R, PosInit, PosPP).
-positionLPP(Pos, Size, [X|R], PosInit, PosPP) :- positionLPP(Pos, Size, R, PosInit, PosPP).
+Gérer destination pas encore explorée
+
+BuildPath
+
+Init openList
+
+Finir a_star (commentaires)
