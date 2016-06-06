1a490dc51187270b074f38d1270f8065637f837b
vivictor14 <victor.vogel@hotmail.fr>
2016-06-06 22:13:09 +0200

    Update decision.pl

--- decision.pl
+++ decision.pl
@@ -53,12 +53,29 @@ allPosition(Element, L, Index) :- allPosition(Element, L, 0, Index).
 % Mise à jour du labyrinthe
 updateLaby([], L, X, Y, Pos, Size, VPx, VPy) :- initLaby(L, X, Y, Pos, Size, VPx, VPy, Laby), nb_setval(labyrinthe, Laby).
 
-% Initialisation du labyrinthe
-initLaby(L, X, Y, Pos, Size, VPx, VPy, [[E|_]|_], Xe, Ye) :- posToCoord(X, Y, Pos, Size, ((VPx * 2 + 1) * (VPy * 2 + 1) - 1), X1, Y1), Xe = X1, Ye = Y1, Pos1 is (Pos + (Xe - X) + Size * (Ye - Y)), elemAtPos(L, Pos1, E), write(1 + Ye).
-initLaby(L, X, Y, Pos, Size, VPx, VPy, [_|R2], Xe, Ye) :- Pos1 is ((VPx * 2 + 1) * (VPy * 2 + 1) - 1), posToCoord(X, Y, Pos, Size, Pos1, X2, _), Xe > X2, Ye1 is Ye + 1, write(2 + Ye), initLaby(L, X, Y, Pos, Size, VPx, VPy, R2, 0, Ye1).
-initLaby(L, X, Y, Pos, Size, VPx, VPy, [[E|R1]|R2], Xe, Ye) :- posToCoord(X, Y, Pos, Size, 0, _, Y1), Ye < Y1, Pos1 is ((VPx * 2 + 1) * (VPy * 2 + 1) - 1), posToCoord(X, Y, Pos, Size, Pos1, X2, _), Xe =< X2,  E is -1, Xe1 is Xe + 1, write(3 + Ye), initLaby(L, X, Y, Pos, Size, VPx, VPy, [R1|R2], Xe1, Ye).
-initLaby(L, X, Y, Pos, Size, VPx, VPy, [[E|R1]|R2], Xe, Ye) :- posToCoord(X, Y, Pos, Size, 0, X1, _), Xe < X1, E is -1, Xe1 is Xe + 1, write(4 + Ye), initLaby(L, X, Y, Pos, Size, VPx, VPy, [R1|R2], Xe1, Ye).
-initLaby(L, X, Y, Pos, Size, VPx, VPy, [[E|R1]|R2], Xe, Ye) :- Pos1 is (Pos + (Xe - X) + Size * (Ye - Y)), elemAtPos(L, Pos1, E), Xe1 is Xe + 1, write(5 + Ye), initLaby(L, X, Y, Pos, Size, VPx, VPy, [R1|R2], Xe1, Ye).
+% Dernière case
+updateLaby(L, X, Y, Pos, Size, VPx, VPy, [], [[E]], Xe, Ye) :- Pos1 is ((VPx * 2 + 1) * (VPy * 2 + 1) - 1), posToCoord(X, Y, Pos, Size, Pos1, X1, Y1), Xe = X1, Ye = Y1, Pos2 is (Pos + (Xe - X) + Size * (Ye - Y)), elemAtPos(L, Pos2, E).
+%updateLaby(L, X, Y, Pos, Size, VPx, VPy, [[E]], [], Xe, Ye) :- Pos1 is ((VPx * 2 + 1) * (VPy * 2 + 1) - 1), posToCoord(X, Y, Pos, Size, Pos1, X1, Y1), Xe >= X1, Ye >= Y1.
+
+
+
+
+updateLaby(L, X, Y, Pos, Size, VPx, VPy, OldLaby, [[E|R1]|R2], Xe, Ye) :- posToCoord(X, Y, Pos, Size, 0, X1, Y1), Xe >= X1, Ye >= Y1, posLastElement(L, Pos1), posToCoord(X, Y, Pos, Size, Pos1, X2, Y2), Xe =< X2, Ye =< Y2, Pos2 is (Pos + (Xe - X) + Size * (Ye - Y)), elemAtPos(L, Pos2, E), Xe1 is Xe + 1, initLaby(L, X, Y, Pos, Size, VPx, VPy, OldLaby, [R1|R2], Xe1, Ye).
+%updateLaby(L, X, Y, Pos, Size, VPx, VPy, OldLaby, [[E|R1]|R2])
+
+
+
+
+
+
+updateLaby(L, X, Y, Pos, Size, VPx, VPy, [[E|R1]|R2], Xe, Ye) :- posToCoord(X, Y, Pos, Size, 0, X1, Y1), Xe >= X1, Ye >= Y1, Pos1 is (Pos + (Xe - X) + Size * (Ye - Y)), elemAtPos(L, Pos1, E), Xe1 is Xe + 1, initLaby(L, X, Y, Pos, Size, VPx, VPy, [R1|R2], Xe1, Ye).
+updateLaby(L, X, Y, Pos, Size, VPx, VPy, [[E|R1]|R2], Xe, Ye) :- E is -1, Xe1 is Xe + 1, initLaby(L, X, Y, Pos, Size, VPx, VPy, [R1|R2], Xe1, Ye).
+
+initLaby(L, X, Y, Pos, Size, VPx, VPy, [[E]], Xe, Ye) :- posToCoord(X, Y, Pos, Size, ((VPx * 2 + 1) * (VPy * 2 + 1) - 1), X1, Y1), Xe = X1, Ye = Y1, Pos1 is (Pos + (Xe - X) + Size * (Ye - Y)), elemAtPos(L, Pos1, E).
+initLaby(L, X, Y, Pos, Size, VPx, VPy, [[E]|R2], Xe, Ye) :- posToCoord(X, Y, Pos, Size, 0, _, Y1), Ye < Y1, Pos1 is ((VPx * 2 + 1) * (VPy * 2 + 1) - 1), posToCoord(X, Y, Pos, Size, Pos1, X2, _), Xe = X2, E is -1, Ye1 is Ye + 1, initLaby(L, X, Y, Pos, Size, VPx, VPy, R2, 0, Ye1).
+initLaby(L, X, Y, Pos, Size, VPx, VPy, [[E]|R2], Xe, Ye) :- Pos1 is ((VPx * 2 + 1) * (VPy * 2 + 1) - 1), posToCoord(X, Y, Pos, Size, Pos1, X2, _), Xe = X2, Pos2 is (Pos + (Xe - X) + Size * (Ye - Y)), elemAtPos(L, Pos2, E), Ye1 is Ye + 1, initLaby(L, X, Y, Pos, Size, VPx, VPy, R2, 0, Ye1).
+initLaby(L, X, Y, Pos, Size, VPx, VPy, [[E|R1]|R2], Xe, Ye) :- posToCoord(X, Y, Pos, Size, 0, X1, Y1), Xe >= X1, Ye >= Y1, Pos1 is (Pos + (Xe - X) + Size * (Ye - Y)), elemAtPos(L, Pos1, E), Xe1 is Xe + 1, initLaby(L, X, Y, Pos, Size, VPx, VPy, [R1|R2], Xe1, Ye).
+initLaby(L, X, Y, Pos, Size, VPx, VPy, [[E|R1]|R2], Xe, Ye) :- E is -1, Xe1 is Xe + 1, initLaby(L, X, Y, Pos, Size, VPx, VPy, [R1|R2], Xe1, Ye).
 
 
 % Transformer position dans la liste en coordonnées dans le labyrinthe
@@ -67,5 +84,21 @@ posToCoord(X, Y, Pos, Size, Pos1, X1, Y1) :- X1 is (X + (Pos1 mod Size) - (Pos m
 % Element à la position donnée
 elemAtPos([E|_], 0, E).
 elemAtPos([_|R], Pos, E) :- Pos1 is Pos - 1, elemAtPos(R, Pos1, E).
+
+% Element aux coordonnées données
+elemAtCoord([[E|_]|_], 0, 0, E).
+elemAtCoord([[_|R1]|R2], X, 0, E) :- X1 is X - 1, elemAtCoord([R1|R2], X1, 0, E). 
+elemAtCoord([_|R2], X, Y, E) :- Y1 is Y - 1, elemAtCoord(R2, X, Y1, E).
+
+% Position du dernier élément
+posLastElement(L, Pos) :- posLastElement(L, 0, Pos).
+posLastElement([], Pos, Pos).
+posLastElement([_|R], Pos0, Pos) :- Pos1 is Pos0 + 1, posLastElement(R, Pos1, Pos).
+
+% Coordonnées du dernier élément
+coordLastElement(Laby, X, Y) :- coordLastElement(Laby, 0, 0, X, Y).
+coordLastElement([], X, Y, X, Y).
+coordLastElement([[]|R2], X0, Y0, X, Y) :- Y1 is Y0 + 1, coordLastElement(R2, X0, Y1, X, Y).
+coordLastElement([[_|R1]|R2], X0, Y0, X, Y) :- X1 is X0 + 1, coordLastElement([R1|R2], X1, Y0, X, Y). 
 
 %initLaby([ 1, 0, 1, 1, 2, 23, 1, 21, 2, 3, 2, 2, 0, 0, 0 ], 2, 1, 7, 5, 2, 1, Laby, 0, 0).
