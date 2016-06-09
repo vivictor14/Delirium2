e6b4c32097bec80981f798e7914bbaee42bd8c21
Zaxyh <zaxyh@hotmail.fr>
2016-06-09 11:11:03 +0200

    Oups bétise

--- seDirigerVers.pl
+++ seDirigerVers.pl
@@ -1,88 +1,449 @@
-/*
-  Définition de l'IA du mineur
-  Le prédicat move/12 est consulté à chaque itération du jeu.
-*/
-
-:- module( decision, [
-	init/1,
-	move/12
+:- module( seDirigerVers, [
+	init_astar/1,
+	seDirigerVers/5
 ] ).
-:- use_module(seDirigerVers).
-:- use_module(errer).
 
-% Initialise les tableaux de l'algorythme A*
-init(_) :- nb_setval(labyrinthe, [[-1]]), seDirigerVers:init_astar(_).
+seDirigerVers(Coordonnee,Fin,Laby,Cout,Action):-
+	initialisationGlobale(Coordonnee,Fin),
+	a_star(Coordonnee,Fin,Laby,Path,Cout),
+	!,
+	getSuite(Coordonnee,Path,Suite),
+	getAction(Coordonnee,Suite,Action).
+	
+	
+getSuite(Depart,[Suite,Depart,[-1,-1]],Suite):- !.
+getSuite(Depart,[_|Reste],Suite):- getSuite(Depart,Reste,Suite).
 
+getAction([X,_],[XSuite,_],Action):-
+	XSuite is X + 1,
+	Action = 1.
+getAction([X,_],[XSuite,_],Action):-
+	XSuite is X - 1,
+	Action = 3.
+getAction([_,Y],[_,YSuite],Action):-
+	YSuite is Y + 1,
+	Action = 4.
+getAction([_,Y],[_,YSuite],Action):-
+	YSuite is Y - 1,
+	Action = 2.
+	
+	
+% Init variables globales
+init_astar(_):- 
+	nb_setval(openList,[]), 
+	nb_setval(closeList,[]).
+	
+initialisationGlobale(Coordonnee,Fin):- 
+	getHeuristicValue(Coordonnee,Fin,H),
+	nb_setval(openList,[[Coordonnee,0,H,[-1,-1]]]), 
+	nb_setval(closeList,[]).
+
+% Predicats A*
+/* 
+  a_star(+CourantState, +FinalState, +Labyrinth, -Path) 
+*/
+% openList vide pas de solution
+a_star(_,_,_,_,_):-
+	nb_getval(openList, []),
+	!,
+	fail.
+	
+% etat final atteint par -1
+a_star([X,Y],_,Laby,Path,Cout):-
+	elemAtCoord(Laby, X, Y, E),
+	E = -1,
+	buildPath([X,Y],Path,Cout).
+	
+% etat final atteint
+a_star([X,Y],[X,Y],_,Path,Cout):-
+	buildPath([X,Y],Path,Cout).
+
+a_star([X,Y],[XFinal,YFinal],Laby,Path,Cout):-
+	extractBestNodeFromOpenList(Node),
+	addNodeToClose(Node),
+	Node = [[X,Y],G,_,_],
+	trouverSuccesseurs([X,Y],Laby,Successeurs),
+	ajouterChemin([X,Y],Successeurs,[XFinal,YFinal],G,Laby),
+	getBestNodeFromOpenList(BestNode),
+	BestNode = [NewState,_,_,_],
+	a_star(NewState,[XFinal,YFinal],Laby,Path,Cout).
+	
+ajouterChemin(_,[],_,_,_):-
+	!.
+ajouterChemin([X,Y],[[]|Reste],Fin,G,_):-
+	!,
+	ajouterChemin([X,Y],Reste,Fin,G,_).
+	
+% test si il n'est pas dans closed et open
+ajouterChemin([X,Y],[Successeur|Reste],Fin,G,Laby):-
+	not(isInOpen(Successeur)),
+	not(isInClose(Successeur)),
+	!,
+	getHeuristicValue(Successeur,Fin,H),
+	%getCValue(C,Successeur,Laby),
+	%GPlus is (G + C),
+	GPlus is (G+1),
+	F is (GPlus + H),
+	NewNode = [Successeur,GPlus,F,[X,Y]],
+	addNodeToOpen(NewNode),
+	ajouterChemin([X,Y],Reste,Fin,G,Laby).
+	
+% test si g(y) > g(N.e)+c(N.e,y)
+ajouterChemin([X,Y],[Successeur|Reste],Fin,G,_):-
+	testBestCostInOpenOrClose([X,Y],Successeur,G,Fin),
+	!,
+	ajouterChemin([X,Y],Reste,Fin,G,_).
+
+% aucun des deux critères n'est possible, ont passe au successeur suivant
+ajouterChemin([X,Y],[_|Reste],Fin,G,_):-
+	ajouterChemin([X,Y],Reste,Fin,G,_).
+	
+/*% 10 de cout pour un rocher
+getCValue(C,[X,Y],Laby):-
+	elemAtCoord(Laby,X,Y,3),
+	!,
+	C is 10.
+% 3 de cout pour de la dirt
+getCValue(C,[X,Y],Laby):-
+	elemAtCoord(Laby,X,Y,1),
+	!,
+	C is 3.
+% 1 pour le reste
+getCValue(C,_,_):-
+	!,
+	C is 1.*/
+	
+% trouver les successeur 
+trouverSuccesseurs([X,Y],Laby,Successeurs):-
+	movementD([X,Y],Laby,CoordD),
+	movementH([X,Y],Laby,CoordH),
+	movementG([X,Y],Laby,CoordG),
+	movementB([X,Y],Laby,CoordB),
+	Successeurs = [CoordD,CoordH,CoordG,CoordB].
+
+movementD([X,Y],Laby,CoordD):-
+	XNew is X+1,
+	possibleMove([XNew,Y],Laby),
+	!,
+	CoordD = [XNew,Y].
+
+movementD(_,_,[]).
+	
+movementH([X,Y],Laby,CoordH):-
+	YNew is Y-1,
+	possibleMove([X,YNew],Laby),
+	!,
+	CoordH = [X,YNew].
+
+movementH(_,_,[]).
+	
+movementB([X,Y],Laby,CoordB):-
+	YNew is Y+1,
+	possibleMove([X,YNew],Laby),
+	!,
+	CoordB = [X,YNew].
+
+movementB(_,_,[]).
+	
+movementG([X,Y],Laby,CoordG):-
+	XNew is X-1,
+	possibleMove([XNew,Y],Laby),
+	!,
+	CoordG = [XNew,Y].
+
+movementG(_,_,[]).	
+
+% Predicats A* spécifique problème
+/* 
+  getHeuristicValue([+XATester,+YATester],[+XFinal,+YFinal],-V)
+*/
+getHeuristicValue([XATester,YATester],[XFinal,YFinal],V):- V is sqrt((XFinal-XATester)*(XFinal-XATester)+(YATester-YFinal)*(YATester-YFinal)).
 
 /*
-  move( +L,+LP, +X,+Y,+Pos, +Size, +CanGotoExit, +Energy,+GEnergy, +VPx,+VPy, -ActionId )
+  extractBestNodeFromOpenList(-Node)
+*/
+%si un seul elem
+extractBestNodeFromOpenList(BestNode):-
+	nb_getval(openList,OpenList),
+	OpenList = [BestNode|[]],
+	!,
+	substractFromOpenList(BestNode).
+
+extractBestNodeFromOpenList(BestNode):-
+	nb_getval(openList,OpenList),
+	OpenList = [Node|ResteOpen],
+	Node=[_,_,F,_],
+	extractBestNodeFromOpenList(Node,BestNode,F,_,ResteOpen),
+	substractFromOpenList(BestNode).
+	
+extractBestNodeFromOpenList(BestNode,BestNode,BestF,BestF,[]):-
+	!.
+extractBestNodeFromOpenList(_,BestNode,F,BestF,[Node|AutreNode]):-
+	Node = [_,_,FNode,_],
+	F > FNode,
+	!,
+	extractBestNodeFromOpenList(Node,BestNode,FNode,BestF,AutreNode).
+extractBestNodeFromOpenList(CourNode,BestNode,F,BestF,[_|AutreNode]):-
+	!,
+	extractBestNodeFromOpenList(CourNode,BestNode,F,BestF,AutreNode).
+	
+/*
+  getBestNodeFromOpenList(-Node)
+*/
+getBestNodeFromOpenList(BestNode):-
+	nb_getval(openList,OpenList),
+	OpenList = [Node|ResteOpen],
+	Node=[_,_,F,_],
+	getBestNodeFromOpenList(Node,BestNode,F,_,ResteOpen).
+	
+getBestNodeFromOpenList(BestNode,BestNode,BestF,BestF,[]):-
+	!.
+getBestNodeFromOpenList(_,BestNode,F,BestF,[Node|AutreNode]):-
+	Node = [_,_,FNode,_],
+	F > FNode,
+	!,
+	getBestNodeFromOpenList(Node,BestNode,FNode,BestF,AutreNode).
+getBestNodeFromOpenList(CourNode,BestNode,F,BestF,[_|AutreNode]):-
+	getBestNodeFromOpenList(CourNode,BestNode,F,BestF,AutreNode).
+
+/*
+  extractNodeFromOpen(+State, -Node)
+*/
+extractNodeFromOpen(State,Node):-
+	nb_getval(openList,OpenList),
+	extractNodeFromOpen(State,Node,OpenList),
+	substractFromOpenList(Node).
+	
+extractNodeFromOpen(_,_,[]):- fail.
+extractNodeFromOpen(State,Node,[Node|_]):-
+	Node = [State,_,_,_].
+extractNodeFromOpen(State,Node,[_|AutreNode]):-
+	extractNodeFromOpen(State,Node,AutreNode).	
+	
+/*
+  getNodeFromOpen(+State, -Node)
+*/
+getNodeFromOpen(State,Node):-
+	nb_getval(openList,OpenList),
+	getNodeFromOpen(State,Node,OpenList).
+	
+getNodeFromOpen(_,_,[]):- fail.
+getNodeFromOpen(State,Node,[Node|_]):-
+	Node = [State,_,_,_].
+getNodeFromOpen(State,Node,[_|AutreNode]):-
+	getNodeFromOpen(State,Node,AutreNode).	
+	
+	
+/*
+  substractFromOpenList(+Node)
+*/
+substractFromOpenList(Node):-
+	nb_getval(openList,OpenList),
+	substractFromOpenList(Node,OpenList,[]).
+	
+substractFromOpenList(Node,[Node|AutreNode],DebutNode):-
+	append(DebutNode,AutreNode,NewOpenList),
+	!,
+	nb_setval(openList,NewOpenList).
+substractFromOpenList(Node,[PasNode|AutreNode],DebutNode):-
+	append(DebutNode,[PasNode],ListeNode),
+	substractFromOpenList(Node,AutreNode,ListeNode).
+	
+/*
+  addNodeToOpen(+Node)
+*/
+addNodeToOpen(Node):-
+	nb_getval(openList,OpenList),
+	append([Node],OpenList,NewOpenList),
+	nb_setval(openList,NewOpenList).
+
+/*
+  extractNodeFromClose(+State, -Node)
+*/
+extractNodeFromClose(State,Node):-
+	nb_getval(closeList,CloseList),
+	extractNodeFromClose(State,Node,CloseList),
+	substractFromCloseList(Node).
+	
+extractNodeFromClose(_,_,[]):- fail.
+extractNodeFromClose(State,Node,[Node|_]):-
+	Node = [State,_,_,_].
+extractNodeFromClose(State,Node,[_|AutreNode]):-
+	extractNodeFromClose(State,Node,AutreNode).
+	
+/*
+  getNodeFromClose(+State, -Node)
+*/
+getNodeFromClose(State,Node):-
+	nb_getval(closeList,CloseList),
+	getNodeFromClose(State,Node,CloseList).
+	
+getNodeFromClose(_,_,[]):- fail.
+getNodeFromClose(State,Node,[Node|_]):-
+	Node = [State,_,_,_].
+getNodeFromClose(State,Node,[_|AutreNode]):-
+	getNodeFromClose(State,Node,AutreNode).
+
+/*
+  substractFromCloseList(+Node)
+*/
+substractFromCloseList(Node):-
+	nb_getval(closeList,CloseList),
+	substractFromCloseList(Node,CloseList,[]).
+	
+substractFromCloseList(Node,[Node|AutreNode],DebutNode):-
+	append(DebutNode,AutreNode,NewCloseList),
+	!,
+	nb_setval(closeList,NewCloseList).
+substractFromCloseList(Node,[PasNode|AutreNode],DebutNode):-
+	append(DebutNode,[PasNode],ListeNode),
+	substractFromCloseList(Node,AutreNode,ListeNode).
+	
+/*
+  addNodeToClose(+Node)
+*/
+addNodeToClose(Node):-
+	nb_getval(closeList,CloseList),
+	append([Node],CloseList,NewCloseList),
+	nb_setval(closeList,NewCloseList).
+	
+/*
+  testBestCostInOpenOrClose(+Pere,+Successeur,+GPere,+Fin)
+*/
+testBestCostInOpenOrClose([X,Y],Successeur,GPere,Fin):-
+	isInOpenWithBestCost(Successeur,GPere),
+	!,
+	extractNodeFromOpen(Successeur,_),
+	GPlus is (GPere + 1),
+	getHeuristicValue(Successeur,Fin,H),
+	F is (GPlus + H),
+	NewNode = [Successeur,GPlus,F,[X,Y]],
+	addNodeToOpen(NewNode).
+	
+testBestCostInOpenOrClose([X,Y],Successeur,GPere,Fin):-
+	isInCloseWithBestCost(Successeur,GPere),
+	!,
+	extractNodeFromClose(Successeur,_),
+	GPlus is (GPere + 1),
+	getHeuristicValue(Successeur,Fin,H),
+	F is (GPlus + H),
+	NewNode = [Successeur,GPlus,F,[X,Y]],
+	addNodeToOpen(NewNode).
+	
+/*
+  isInOpenWithBestCost(+Successeur,+GPere)
+*/
+isInOpenWithBestCost(Successeur,GPere):-
+	nb_getval(openList,OpenList),
+	isInOpenWithBestCost(Successeur,GPere,OpenList).
+	
+isInOpenWithBestCost(_,_,[]):- fail.
+	
+isInOpenWithBestCost(Successeur,GPere,[Node|_]):-
+	Node = [Successeur,G,_,_],
+	GPlus is (GPere + 1),
+	GPlus < G,
+	!.
+	
+isInOpenWithBestCost(Successeur,GPere,[_|AutreNode]):-
+	isInOpenWithBestCost(Successeur,GPere,AutreNode).
+
+
+	
+/*
+  isInCloseWithBestCost(+Successeur,+GPere)
+*/
+isInCloseWithBestCost(Successeur,GPere):-
+	nb_getval(closeList,CloseList),
+	isInCloseWithBestCost(Successeur,GPere,CloseList).
+	
+isInCloseWithBestCost(_,_,[]):- fail.
+	
+isInCloseWithBestCost(Successeur,GPere,[Node|_]):-
+	Node = [Successeur,G,_,_],
+	GPlus is (GPere + 1),
+	GPlus < G,
+	!.
+	
+isInCloseWithBestCost(Successeur,GPere,[_|AutreNode]):-
+	isInCloseWithBestCost(Successeur,GPere,AutreNode).
+	
+/*
+  buildPath(+Coordonnee,-Path,-Cout)
+*/
+buildPath([X,Y],Path,Cout):-
+	getCout([X,Y],Cout),
+	buildPath([X,Y],PathSansPere,[]),
+	append([[X,Y]],PathSansPere,Path).
+
+buildPath([-1,-1],Path,Path).
+
+buildPath([X,Y],Path,CourPath):-
+	isInOpen([X,Y]),
+	extractNodeFromOpen([X,Y],Node),
+	Node = [[X,Y],_,_,Pere],
+	append(CourPath,[Pere],NewPath),
+	buildPath(Pere,Path,NewPath).
+	
+buildPath([X,Y],Path,CourPath):-
+	isInClose([X,Y]),
+	extractNodeFromClose([X,Y],Node),
+	Node = [[X,Y],_,_,Pere],
+	append(CourPath,[Pere],NewPath),
+	buildPath(Pere,Path,NewPath).
+	
+	
+/*
+  getCout(+Coordonne,-Cout)
 */
 
-% Mettre à jour le labyrinthe avant de choisir le mouvement
-move(L, _, X, Y, Pos, Size, CGE, _, _, _, _, Action) :- nb_getval(labyrinthe, Laby), updateLaby(Laby, L, X, Y, Pos, Size), nb_getval(labyrinthe, Laby1), write(Laby1), nl, move(X, Y, CGE, Laby1, Action).
+getCout([X,Y],Cout):-
+	isInOpen([X,Y]),
+	getNodeFromOpen([X,Y],Node),
+	Node = [_,_,Cout,_].
+	
+getCout([X,Y],Cout):-
+	isInClose([X,Y]),
+	getNodeFromClose([X,Y],Node),
+	Node = [_,_,Cout,_].
+	
+/*
+  isInOpen(+Coordonnee)
+*/
+isInOpen(Successeur):-
+	nb_getval(openList,OpenList),
+	isInOpen(Successeur,OpenList).
+	
+isInOpen(_,[]):- fail.
+	
+isInOpen(Successeur,[Node|_]):-
+	Node = [Successeur,_,_,_],
+	!.
+	
+isInOpen(Successeur,[_|AutreNode]):-
+	isInOpen(Successeur,AutreNode).
 
-% S'arrêter sur la sortie
-move(X, Y, 1, Laby, 0) :- elemAtCoord(Laby, X, Y, 21).
-
-% Se diriger vers la sortie
-move(X, Y, 1, Laby, Action) :- premiereOccurence(Laby, 21, X1, Y1), seDirigerVers([X, Y], [X1, Y1], Laby, _, Action), write('Je me dirige vers la sortie'), nl.
-
-% Se diriger vers un diamant
-move(X, Y, 0, Laby, Action) :- meilleureOption(X, Y, Laby, 2, Action), write('Je me dirige vers un diamant'), nl.
-
-% Errer
-move(X, Y, _, Laby, Action) :- errer(X, Y, Laby, Action), write('J erre'), nl.
-
-% Mise à jour du labyrinthe
-updateLaby(OldLaby, L, X, Y, Pos, Size) :- coordLastElement(OldLaby, X1, Y1), posLastElement(L, Pos1), updateLaby(L, X, Y, X1, Y1, Pos, Pos1, Size, OldLaby, Laby, 0, 0), nb_setval(labyrinthe, Laby).
-
-updateLaby(L, X, Y, X1, Y1, Pos, Pos1, Size, _, [[E]], Xe, Ye) :- Xe >= X1, Ye >= Y1, posToCoord(X, Y, Pos, Size, Pos1, X2, Y2), Xe = X2, Ye = Y2, elemAtPos(L, Pos1, E).
-updateLaby(_, X, Y, X1, Y1, Pos, Pos1, Size, OldLaby, [[E]], Xe, Ye) :- Xe = X1, Ye = Y1, posToCoord(X, Y, Pos, Size, Pos1, X2, Y2), Xe >= X2, Ye >= Y2, elemAtCoord(OldLaby, X1, Y1, E).
-updateLaby(_, X, Y, X1, Y1, Pos, Pos1, Size, _, [[-1]], Xe, Ye) :- Xe >= X1, Ye >= Y1, posToCoord(X, Y, Pos, Size, Pos1, X2, Y2), Xe >= X2, Ye >= Y2.
-updateLaby(L, X, Y, X1, Y1, Pos, Pos1, Size, OldLaby, [[E]|R2], Xe, Ye) :- Xe >= X1, posToCoord(X, Y, Pos, Size, Pos1, X2, Y2), Xe = X2, Ye =< Y2, posToCoord(X, Y, Pos, Size, 0, _, Y3), Ye >= Y3, Pos2 is (Pos + (Xe - X) + Size * (Ye - Y)), elemAtPos(L, Pos2, E), Ye1 is Ye + 1, updateLaby(L, X, Y, X1, Y1, Pos, Pos1, Size, OldLaby, R2, 0, Ye1).
-updateLaby(L, X, Y, X1, Y1, Pos, Pos1, Size, OldLaby, [[E]|R2], Xe, Ye) :- Xe = X1, Ye =< Y1, posToCoord(X, Y, Pos, Size, Pos1, X2, _), Xe >= X2, elemAtCoord(OldLaby, Xe, Ye, E), Ye1 is Ye + 1, updateLaby(L, X, Y, X1, Y1, Pos, Pos1, Size, OldLaby, R2, 0, Ye1).
-updateLaby(L, X, Y, X1, Y1, Pos, Pos1, Size, OldLaby, [[-1]|R2], Xe, Ye) :- Xe >= X1, posToCoord(X, Y, Pos, Size, Pos1, X2, _), Xe >= X2, Ye1 is Ye + 1, updateLaby(L, X, Y, X1, Y1, Pos, Pos1, Size, OldLaby, R2, 0, Ye1).
-updateLaby(L, X, Y, X1, Y1, Pos, Pos1, Size, OldLaby, [[E|R1]|R2], Xe, Ye) :- posToCoord(X, Y, Pos, Size, Pos1, X2, Y2), Xe =< X2, Ye =< Y2, posToCoord(X, Y, Pos, Size, 0, X3, Y3), Xe >= X3, Ye >= Y3, Pos2 is (Pos + (Xe - X) + Size * (Ye - Y)), elemAtPos(L, Pos2, E), Xe1 is Xe + 1, updateLaby(L, X, Y, X1, Y1, Pos, Pos1, Size, OldLaby, [R1|R2], Xe1, Ye).
-updateLaby(L, X, Y, X1, Y1, Pos, Pos1, Size, OldLaby, [[E|R1]|R2], Xe, Ye) :- Xe =< X1, Ye =< Y1, elemAtCoord(OldLaby, Xe, Ye, E), Xe1 is Xe + 1, updateLaby(L, X, Y, X1, Y1, Pos, Pos1, Size, OldLaby, [R1|R2], Xe1, Ye).
-updateLaby(L, X, Y, X1, Y1, Pos, Pos1, Size, OldLaby, [[-1|R1]|R2], Xe, Ye) :- Xe1 is Xe + 1, updateLaby(L, X, Y, X1, Y1, Pos, Pos1, Size, OldLaby, [R1|R2], Xe1, Ye).
-
-test(Action) :- init(_), meilleureOption(1, 1, [[5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5], [5, 22, 0, 2, 0, 0, 6, 0, 0, 0, 0, 5], [5, 6, 6, 6, 6, 0, 6, 0, 6, 0, 6, 5], [5, 3, 0, 2, 6, 0, 6, 0, 0, 0, 0, 5], [5, 6, 1, 6, 6, 0, 0, 0, 6, 0, 6, 5], [5, 0, 0, 0, 6, 6, 0, 6, 6, 0, 0, 5], [5, 2, 0, 0, 0, 0, 0, 6, 0, 0, 21, 5], [5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5]], 2, Action).
-test2(Action) :- init(_), meilleureOption(0, 0, [[22, 0, 0], [2, 0, 0], [1, 1, 1]], 2, Action).
-
-% Meilleure option possible
-meilleureOption(X, Y, Laby, Elem, Action) :- meilleureOption(X, Y, Laby, Laby, 0, 0, Elem, 42, 0, Action).
-
-meilleureOption(X, Y, Laby, [[Elem]], X0, Y0, Elem, Min, _, Action) :- seDirigerVers([X, Y], [X0, Y0], Laby, Cout, Action), Cout =< Min.
-meilleureOption(_, _, _, [[_]], _, _, _, _, Action, Action) :- not(Action = 0).
-meilleureOption(X, Y, Laby, [[]|R2], _, Y0, Elem, Min, Action0, Action) :- Y1 is Y0 + 1, meilleureOption(X, Y, Laby, R2, 0, Y1, Elem, Min, Action0, Action).
-meilleureOption(X, Y, Laby, [[Elem|R1]|R2], X0, Y0, Elem, Min, _, Action) :- seDirigerVers([X, Y], [X0, Y0], Laby, Cout, Action1), Cout =< Min, X1 is X0 + 1, meilleureOption(X, Y, Laby, [R1|R2], X1, Y0, Elem, Cout, Action1, Action).
-meilleureOption(X, Y, Laby, [[_|R1]|R2], X0, Y0, Elem, Min, Action0, Action) :- X1 is X0 + 1, meilleureOption(X, Y, Laby, [R1|R2], X1, Y0, Elem, Min, Action0, Action).
-
-% Transformer position dans la liste en coordonnées dans le labyrinthe
-posToCoord(X, Y, Pos, Size, Pos1, X1, Y1) :- X1 is (X + (Pos1 mod Size) - (Pos mod Size)), Y1 is (Y + (Pos1 // Size) - (Pos // Size)).
-
-% Element à la position donnée
-elemAtPos([E|_], 0, E).
-elemAtPos([_|R], Pos, E) :- Pos1 is Pos - 1, elemAtPos(R, Pos1, E).
-
+/*
+  isInClose(+Coordonnee)
+*/
+isInClose(Successeur):-
+	nb_getval(closeList,CloseList),
+	isInClose(Successeur,CloseList).
+	
+isInClose(_,[]):- fail.
+	
+isInClose(Successeur,[Node|_]):-
+	Node = [Successeur,_,_,_],
+	!.
+	
+isInClose(Successeur,[_|AutreNode]):-
+	isInClose(Successeur,AutreNode).
+	
 % Element aux coordonnées données
 elemAtCoord([[E|_]|_], 0, 0, E).
-elemAtCoord([[_|R1]|R2], X, 0, E) :- X > 0, X1 is X - 1, elemAtCoord([R1|R2], X1, 0, E). 
-elemAtCoord([_|R2], X, Y, E) :- Y > 0, Y1 is Y - 1, elemAtCoord(R2, X, Y1, E).
+elemAtCoord([[_|R1]|R2], X, 0, E):- X > 0, X1 is X - 1, elemAtCoord([R1|R2], X1, 0, E). 
+elemAtCoord([_|R2], X, Y, E):- Y > 0, Y1 is Y - 1, elemAtCoord(R2, X, Y1, E).
 
-% Position du dernier élément
-posLastElement(L, Pos) :- posLastElement(L, 0, Pos).
-posLastElement([_], Pos, Pos).
-posLastElement([_|R], Pos0, Pos) :- Pos1 is Pos0 + 1, posLastElement(R, Pos1, Pos).
-
-% Coordonnées du dernier élément
-coordLastElement(Laby, X, Y) :- coordLastElement(Laby, 0, 0, X, Y).
-coordLastElement([[_]], X, Y, X, Y).
-coordLastElement([[]|R2], _, Y0, X, Y) :- Y1 is Y0 + 1, coordLastElement(R2, 0, Y1, X, Y).
-coordLastElement([[_|R1]|R2], X0, Y0, X, Y) :- X1 is X0 + 1, coordLastElement([R1|R2], X1, Y0, X, Y).
-
-% Trouver la première occurence d'un élément dans le labyrinthe
-premiereOccurence([[Element|_]|_], Element, X, Y, X, Y).
-premiereOccurence([[]|R], Element, _, Y0, X, Y) :- Y1 is Y0 + 1, X1 is 0, premiereOccurence(R, Element, X1, Y1, X, Y).
-premiereOccurence([[_|R1]|R2], Element, X0, Y0, X, Y) :- X1 is X0 + 1, premiereOccurence([R1|R2], Element, X1, Y0, X, Y).
-premiereOccurence(Laby, Element, X, Y) :- premiereOccurence(Laby, Element, 0, 0, X, Y).
+% Prochaine position possible
+possibleMove([X,Y], Laby):- elemAtCoord(Laby,X,Y, E), E =< 3.
+possibleMove([X,Y], Laby):- elemAtCoord(Laby,X,Y, E), E = 21.
