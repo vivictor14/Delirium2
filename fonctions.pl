11c19233826fd153d0a5f14cc6507f323167338c
vivictor14 <victor.vogel@hotmail.fr>
2016-06-11 21:59:19 +0200

    Add files via upload

--- fonctions.pl
+++ fonctions.pl
@@ -1,4 +1,3 @@
-
 :- module( fonctions, [
 	meilleureOption/4,
 	posToCoord/7,
@@ -56,4 +55,3 @@ premiereOccurence([[Element|_]|_], Element, X, Y, X, Y) :- !.
 premiereOccurence([[]|R], Element, _, Y0, X, Y) :- Y1 is Y0 + 1, X1 is 0, premiereOccurence(R, Element, X1, Y1, X, Y).
 premiereOccurence([[_|R1]|R2], Element, X0, Y0, X, Y) :- X1 is X0 + 1, premiereOccurence([R1|R2], Element, X1, Y0, X, Y).
 premiereOccurence(Laby, Element, X, Y) :- premiereOccurence(Laby, Element, 0, 0, X, Y).
-
